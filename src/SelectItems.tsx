83f2d463d52c7492486ab149d269a851129f8273
Gaurav <07gaurav04kumar03@gmail.com>
2025-03-25 00:56:55 +0530

    Updated Branch list

--- src/SelectItems.tsx
+++ src/SelectItems.tsx
@@ -2,17 +2,17 @@ import { SelectItem } from '@/components/ui/select';
 
 export const SelectItems = () => (
   <>
+    <SelectItem value="AeroSpace">Aerospace Engineering</SelectItem>
+    <SelectItem value="Civil">Civil Engineering</SelectItem>
     <SelectItem value="CSE">Computer Science and Engineering</SelectItem>
-    <SelectItem value="CSSE">Computer Science and Systems Engineering</SelectItem>
     <SelectItem value="CSCE">Computer Science and Communication Engineering</SelectItem>
-    <SelectItem value="ETC">Electronics and Telecommunication Engineering</SelectItem>
-    <SelectItem value="EE">Electrical Engineering</SelectItem>
-    <SelectItem value="EEE">Electronics and Electrical Engineering</SelectItem>
+    <SelectItem value="CSSE">Computer Science and Systems Engineering</SelectItem>
     <SelectItem value="ECSE">Electronics and Computer Science Engineering</SelectItem>
+    <SelectItem value="EEE">Electronics and Electrical Engineering</SelectItem>
+    <SelectItem value="EE">Electrical Engineering</SelectItem>
+    <SelectItem value="ETC">Electronics and Telecommunication Engineering</SelectItem>
+    <SelectItem value="IT">Information Technology</SelectItem>
     <SelectItem value="Mech">Mechanical Engineering</SelectItem>
     <SelectItem value="Mechatronics">Mechatronics Engineering</SelectItem>
-    <SelectItem value="Civil">Civil Engineering</SelectItem>
-    <SelectItem value="IT">Information Technology</SelectItem>
-    <SelectItem value="AeroSpace">Aerospace Engineering</SelectItem>
   </>
 );
